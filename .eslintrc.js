51606b4eda28c40fb769e817226a25e16a951397
Alexander Kalinin <alex@kalinin.uk>
2019-09-16 14:08:12 +0100

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		node: true,
+		es6: true,
+	},
+	plugins: [
+		"jest"
+	],
+	extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		"no-console": 1,
+		'no-unused-vars': ["error", { "args": "none" }]
+	}
+}
